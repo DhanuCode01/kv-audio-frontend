e56b8b19bc6521c31ddda62fcc33f94a970e79e8
DhanuCode01 <lakshanshanaka327@gmail.com>
2025-02-07 00:58:37 +0530

    FirstDay👌

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import ProductCard  from './Components/productCard.jsx'
+
+function App() {
+
+
+  return (
+    <>
+     <ProductCard name="Audio Setup" price="325.00"
+     imgURL="https://media.istockphoto.com/id/637824876/photo/headphone-with-other-professional-audio-studio-equipments.jpg?s=612x612&w=0&k=20&c=MJZn-IuwYwS08DGTLTcsRvxqvf6M6ONqtB4tMuVStL0="/>
+  
+    <ProductCard name="speaker" price="25000"
+    imgURL="https://media.istockphoto.com/id/487205823/photo/audio-speakers-isolated.jpg?s=612x612&w=0&k=20&c=HIt9kL-alZYmsDxjWseWJiuSg0ymxON9_uNJRkzEg4c="/>
+
+    </>
+  )
+}
+
+export default App
